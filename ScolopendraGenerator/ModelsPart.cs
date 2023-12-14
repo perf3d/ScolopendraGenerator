2b1efa30e4a8007f99374534b747dd5e1b54c456
Mole <Mole@MSI>
2023-12-14 11:51:13 +0300

    Добавьте файлы проекта.

---
+++ ScolopendraGenerator/ModelsPart.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace scolopendra;
+public class ModelsPart
+{
+    public readonly string? pathToModel=null;
+    public readonly int? trisCount = null;
+    public readonly int? lengthX = null;
+    ModelsPart(string path)
+    {
+        pathToModel = path;
+        trisCount = getTrisCount(path);
+        lengthX = getLengthX(path);
+    }
+
+    private int getTrisCount(string path)
+    {
+
+    }
+
+    private int getLengthX(string path)
+    {
+
+    }
+}
